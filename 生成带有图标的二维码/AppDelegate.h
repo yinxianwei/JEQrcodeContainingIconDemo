900d741334eed3633aaeb4bf38202a351a8505fa
尹现伟 <yinxianwei@qq.com>
2015-01-07 16:50:47 +0800

    Initial Commit

---
+++ 生成带有图标的二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  生成带有图标的二维码
+//
+//  Created by 尹现伟 on 15-1-7.
+//  Copyright (c) 2015年 DNE Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
